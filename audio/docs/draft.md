c12030b8dd825bd75f3395c2dc6b3017e305158d
YPBlib <ypb1024@foxmail.com>
2018-07-20 04:20:33 +0800

    playing audio may not interrupt draw-while

---
+++ audio/docs/draft.md
@@ -0,0 +1,10 @@
+[opengl中不中断draw 循环播放声音-StackOverflow](https://stackoverflow.com/questions/42892254/playing-audio-without-freezing-draw-loop-in-opengl) 
+
+
+
+[上述需求在OpenAL中的实现-StackOverflow](https://stackoverflow.com/questions/15064742/play-stream-in-openal-library)
+
+
+
+
+
--- audio/main.cpp
+++ audio/main.cpp
@@ -1,26 +1,17 @@
-
+#include<string>
 #include<windows.h>
 #include<AL/alut.h>
 #include<al.h>
-#include<alc.h>
-#include<efx.h>
-#include<efx-creative.h>
-#include<EFX-Util.h>
-#include<xram.h>
+//#include<alc.h>
+//#include<efx.h>
+//#include<efx-creative.h>
+//#include<EFX-Util.h>
+//#include<xram.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-char** plt;
-
-
-
-
-/*
-* This program loads and plays a variety of files.
-*/
-
-static void playFile(const char *fileName)
+static void playUtil(const char *fileName)
 {
 	ALuint buffer;
 	ALuint source;
@@ -51,7 +42,6 @@ static void playFile(const char *fileName)
 		exit(EXIT_FAILURE);
 	}
 
-	/* Check every 0.1 seconds if the sound is still playing. */
 	do
 	{
 		alutSleep(0.1f);
@@ -59,57 +49,29 @@ static void playFile(const char *fileName)
 	} while (status == AL_PLAYING);
 }
 
-int play(int argc, char **argv)
+void play(int argc, char **argv)
 {
-	/* Initialise ALUT and eat any ALUT-specific commandline flags. */
-	if (!alutInit(&argc, argv))
-	{
-		ALenum error = alutGetError();
-
-		fprintf(stderr, "%s\n", alutGetErrorString(error));
-		exit(EXIT_FAILURE);
-	}
-
-	/* Check for correct usage. */
-	if (argc != 2)
-	{
-		fprintf(stderr, "usage: playfile <fileName>\n");
-		alutExit();
-		exit(EXIT_FAILURE);
-	}
-
-	/* If everything is OK, play the sound file and exit when finished. */
-	playFile(argv[1]);
-
-	if (!alutExit())
-	{
-		ALenum error = alutGetError();
-
-		fprintf(stderr, "%s\n", alutGetErrorString(error));
-		exit(EXIT_FAILURE);
-	}
-
 	alutInit(&argc, argv);
-	ALuint helloBuffer, helloSource;
-	helloBuffer = alutCreateBufferHelloWorld();
-	alGenSources(1, &helloSource);
-	alSourcei(helloSource, AL_BUFFER, helloBuffer);
-	alSourcePlay(helloSource);
-	alutSleep(1);
+	playUtil(argv[1]);
 	alutExit();
-	return EXIT_SUCCESS;
 }
 
 
-int main()
+void test(std::string s)
 {
 	unsigned int audioId_1;
 	alGenSources(1, &audioId_1);
 	alSource3f(audioId_1, AL_POSITION, 45.0f, 5.0f, -18.0f);
 	alSourcef(audioId_1, AL_GAIN, 0.8f);
-	plt = (char**)malloc(2 * sizeof(char*));
-	plt[1] = "D:\\ProgramsD\\matlab\\toolbox\\sl3d\\vrealm\\program\\sounds\\bubble.wav";
+	char** plt = new char*[2];
+	plt[1] = const_cast<char*> (s.c_str());
 	play(2, plt);
+	delete[] plt;
+}
+
+int main()
+{
+	test("D:\\code\\happy-bird\\audio\\sounds\\bubble.wav");
 	return 0;
 }
 
